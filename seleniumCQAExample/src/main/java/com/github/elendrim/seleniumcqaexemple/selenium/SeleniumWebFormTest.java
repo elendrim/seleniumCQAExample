2a9d5b874dbd84ac866f180417955e3d7145e12b
goliver <goliver@LPGOL01.centro.fr>
2023-03-24 23:16:35 +0100

    initial commit

--- seleniumCQAExample/src/main/java/com/github/elendrim/seleniumcqaexemple/selenium/SeleniumWebFormTest.java
+++ seleniumCQAExample/src/main/java/com/github/elendrim/seleniumcqaexemple/selenium/SeleniumWebFormTest.java
@@ -1,4 +1,4 @@
-package com.github.elendrim.seleniumcqaexemple.wikipedia;
+package com.github.elendrim.seleniumcqaexemple.selenium;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,27 +13,27 @@ import seleniumcqa.WebDriverBot;
 import seleniumcqa.WebDriverBotImpl;
 
 public class SeleniumWebFormTest {
-	
+
 	@Test
 	public void eightComponents() {
-		
+
 		WebDriver driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(0));
+
 		WebDriverBot webDriverBot = new WebDriverBotImpl(driver);
 		webDriverBot.visit("https://www.selenium.dev/selenium/web/web-form.html");
-		
+
 		String title = webDriverBot.getTitle();
-        assertEquals("Web form", title);
+		assertEquals("Web form", title);
 
-        webDriverBot.find(By.name("my-text")).sendKeys("Selenium");
-        webDriverBot.find(By.cssSelector("button")).click();
-        
-        String value = webDriverBot.find(By.id("message")).getText();
-        
-        assertEquals("Received!", value);
+		webDriverBot.find(By.name("my-text")).sendKeys("Selenium");
+		webDriverBot.find(By.cssSelector("button")).click();
 
-        driver.quit();
+		String value = webDriverBot.find(By.id("message")).getText();
+
+		assertEquals("Received!", value);
+
+		driver.quit();
 	}
 
 }
